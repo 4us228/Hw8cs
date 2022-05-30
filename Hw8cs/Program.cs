f5060e4787582c5dfbcb23477a7dd15ee73ba13e
chusi <4us.in.ua@gmail.com>
2022-05-30 16:17:16 +0300

    Времени ни на что нет

--- Hw8cs/Program.cs
+++ Hw8cs/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,30 @@ namespace Hw8cs
     {
         static void Main(string[] args)
         {
+            
+           
+            Type type = typeof(DateTime);
+            PropertyInfo property1 = type.GetProperty("Now");
+            if (property1.CanRead)
+            {
+                DateTime time=(DateTime)property1.GetValue(DateTime.Now);
+                Console.WriteLine($"Time now -> {time} ");
+            }
+            PropertyInfo property2 = type.GetProperty("UtcNow");
+            if (property2.CanRead)
+            {
+                DateTime time=(DateTime)property2.GetValue(DateTime.UtcNow);
+                Console.WriteLine($"UtcNow -> {time} ");
+            }
+            PropertyInfo property3 = type.GetProperty("Today");
+            if (property3.CanRead)
+            {
+                DateTime time=(DateTime)property3.GetValue(DateTime.Today);
+                Console.WriteLine($"Today -> {time} ");
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
